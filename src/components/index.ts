c9707baba470363557853c41c57e0f8cd0374226
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-07-09 17:13:47 -0700

    💄 style: set min width to 80px

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,11 @@ export { default as Button } from './base/Button.astro'
 export { default as Card } from './base/Card.astro'
 export { default as Collapse } from './base/Collapse.astro'
 export { default as Kbd } from './base/Kbd.astro'
+export { default as ProfileLinks } from './base/ProfileLinks.astro'
 
 // Layout
 export { default as BaseHead } from './layout/BaseHead.astro'
 export { default as Footer } from './layout/Footer.astro'
-export { default as LinkNav } from './layout/LinkNav.astro'
 export { default as MenuNav } from './layout/MenuNav.astro'
 export { default as Section } from './layout/Section.astro'
 
@@ -19,7 +19,7 @@ export { default as TOC } from './blog/TOC.astro'
 export { default as SearchBox } from './search/SearchBox.astro'
 
 // Home
-export { default as About } from './home/About.astro'
+export { default as Profile } from './home/Profile.astro'
 export { default as Pronunciation } from './home/Pronunciation.astro'
 export { default as SkillLayout } from './home/SkillLayout.astro'
 
