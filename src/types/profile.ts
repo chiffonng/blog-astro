e40069b0a967e47e50f30f45b349c8031a530957
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-07-16 21:11:41 -0700

    ♻️ refactor: about frontmatter -> site.config.ts

--- src/types/profile.ts
+++ src/types/profile.ts
@@ -2,15 +2,25 @@ import { z } from 'astro/zod'
 
 import { audioPathSchema, ProfileLinkConfigSchema } from '@/types'
 
-// Doc: https://zod.dev/json-schema#object-schemas
-export const aboutSchema = z.object({
-  name: z.string(), // Full name, and prefix (e.g. Dr., Prof., etc.)
-  tagline: z.string().max(80), // One-line introduction
-  location: z.string(),
+/**
+ * @description Schema for profile configuration including personal info
+ */
+export const ProfileConfigSchema = z.object({
+  /** Full name, including prefix like Dr. and Prof. */
+  name: z.string().describe('Full name, including prefix like Dr. and Prof.'),
+
+  /** One line introduction, max 80 characters */
+  tagline: z.string().max(80).describe('One-line introduction'),
+
+  /** Your current location, optional */
+  location: z.string().optional(),
+
+  /** Your phone number, optional. Accept international format with + and () */
   phone: z
     .string()
     .regex(/^[+]?[\d\s().-]{7,22}$/, {
-      message: 'Invalid phone number format. Examples: +1 (628) 900-6301, +44 20 7946 0958'
+      message:
+        'Invalid phone number format. Examples: +1 (628) 900-6301, +44 20 7946 0958. Brackets () and plus + are optional'
     })
     .refine(
       (val) => {
@@ -22,18 +32,37 @@ export const aboutSchema = z.object({
       { message: 'Phone number must contain 7-15 digits' }
     )
     .optional(),
+
+  /** Your preferred pronouns, optional */
   pronouns: z.string().max(20).optional(),
-  pronunciation: z.string().optional(),
-  pronunciationAudioPath: audioPathSchema.optional()
+
+  /** Optional short text to guide viewers how to pronounce your name */
+  pronunciation: z
+    .string()
+    .optional()
+    .describe('Optional short text to guide viewers how to pronounce your name'),
+
+  /** Optional path to an audio of how to pronounce your name */
+  pronunciationAudioPath: audioPathSchema
+    .optional()
+    .describe('Optional path to an audio of how to pronounce your name'),
+
+  /**
+   * @docs Profile links configuration, validated by Zod schema
+   * @link See 'src/types/profile-links.ts' for supported profile links
+   * @example
+   * Object "links" accept two formats: key-value and key-object
+   * links: {
+   *  resume: 'Valid URL or local path to your resume'
+   *  bluesky: {
+   *    url: 'yourusername.bsky.social'
+   *    label: 'text to override how it appears in profile link bar'
+   *  }
+   * }
+   * @note If you don't want to override, use key-value format instead of key-object format
+   */
+  links: ProfileLinkConfigSchema.describe('Profile links configuration test')
 })
 
-/**
- * @description Schema for profile configuration
- */
-export const ProfileConfigSchema = z.object({
-  /** Profile links configuration */
-  links: ProfileLinkConfigSchema.describe('Profile links configuration')
-})
-
-export type AboutType = z.infer<typeof aboutSchema>
+// This results in long popup for intellisense
 export type ProfileConfig = z.infer<typeof ProfileConfigSchema>
