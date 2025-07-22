709986803d108010951af68265dbd1966df3e489
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-07-22 16:29:28 -0700

    🚚 chore: localize blog actions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,8 +5,3 @@ export { getTheme, listenThemeChange, setTheme, showToast } from './set-theme'
 export { dedupPreserveCase, dedupLowerCase, stripHtml } from './string-manipulation'
 export { processProfileLinks } from './process-links'
 export { isMac, modifierKey } from './keyboard'
-
-// Project
-export { getProjectLinks, hasProjectContent, getProjectDescription } from './projects'
-
-export { createNavigationHelpers } from './menu-helper'
